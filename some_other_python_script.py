aab5926f4a2fa0980b05d2b362293ecaf28c5fbf
Abad <n795d@odcf-worker02.inet.dkfz-heidelberg.de>
2023-03-09 16:10:17 +0100

    random upload

--- some_other_python_script.py
+++ some_other_python_script.py
@@ -1,1 +1,5 @@
-print("Hello my name is Nick")
+def some_function(some_list, some_number):
+    new_list = []
+    for idx in range(len(some_list)):
+        new_list.append(some_list[idx] + some_number)
+    return new_list
