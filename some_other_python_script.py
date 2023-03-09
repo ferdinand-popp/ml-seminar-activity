a029276d94eaed6139af0c4a8b1bed8965e1d0ba
Abad <n795d@odcf-worker02.inet.dkfz-heidelberg.de>
2023-03-09 16:13:43 +0100

    test

--- some_other_python_script.py
+++ some_other_python_script.py
@@ -1,5 +1,5 @@
 def some_function(some_list, some_number):
-    new_list = []
+    new_list_for_person_a = []
     for idx in range(len(some_list)):
-        new_list.append(some_list[idx] + some_number)
-    return new_list
+        new_list_for_person_a.append(some_list[idx] + some_number)
+    return new_list_for_person_a
